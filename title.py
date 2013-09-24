cc01f606e460d0849d337ddc966ed42159ad6fe4
JonathanMeans <sf1519@hotmail.com>
2013-09-23 18:40:56 -0700

    Added comments

--- title.py
+++ title.py
@@ -1,39 +1,44 @@
-#This program displays the events
-import worm_obj
-background_image_filename = 'background.jpg'
-mouse_image_filename = 'mouse.jpg'
+#Basic Worm game. Uses PyGame library.
 
+import worm_obj
 import pygame
 from pygame.locals import *
 from sys import exit
 import os
 
+#Set up everything
 pygame.init()
+
 pygame.mixer.init(channels=1)
+bite = pygame.mixer.Sound('bite.ogg')
+pygame.mixer.music.load('goldberg.ogg')
 
 clock = pygame.time.Clock()
+delay = 300
+pygame.time.set_timer(USEREVENT+1, delay)
 
 SCREEN_SIZE = (300, 300)
 screen = pygame.display.set_mode(SCREEN_SIZE, 0, 32)
-testname = os.path.join('Worm', 'bite.wav')
 pygame.display.set_caption("Worm")
+
+#These variables will be used for positioning text
+x = SCREEN_SIZE[0]/2 - 85
+y = 2 * SCREEN_SIZE[1]/5 - 30
 
 font = pygame.font.SysFont("arial", 60);
 font1 = pygame.font.SysFont("arial", 20);
 font_height = font.get_linesize()
+
 title_worm = worm_obj.TitleWorm()
-
-bite = pygame.mixer.Sound('bite.ogg')
-delay = 300
-pygame.time.set_timer(USEREVENT+1, delay)
-pygame.mixer.music.load('goldberg.ogg')
-
-state = 'title'
 food = worm_obj.Food()
 
+state = 'title'
+
+# Main loop
 while True:
     for event in pygame.event.get():
         if state == 'title' or state == 'end':
+            # Start/resume game when player presses a key
             if event.type == KEYDOWN:
                 state = 'game'
                 game_worm = worm_obj.Worm()
@@ -42,42 +47,52 @@ while True:
                 score = 0
                 pygame.mixer.music.set_volume(1.0)
                 pygame.mixer.music.play(-1, 0)
+                
         if state == 'game':
+            #Move worm every [delay] ms
             if event.type == USEREVENT+1:
                 game_worm.move()
+            #Respond to keystrokes
             if event.type == KEYDOWN:
                 game_worm.change_dir()
+                
         if event.type == QUIT:
             pygame.quit()
             exit()
         
-    screen.fill((245, 241, 222))
-    x = SCREEN_SIZE[0]/2 - 85
-    y = 2 * SCREEN_SIZE[1]/5 - 30
 
+    screen.fill((245, 241, 222))
+    
     if state == 'title':
         screen.blit(font.render("WORM!", True, (100,100,0)),(x,y))
         screen.blit(font1.render("Press any key to play.", True, (245, 0, 20)),
                     (x + 7, y+font_height+20))
+        
+        #Move the worm object around the text
         pygame.time.wait(300)
         title_worm.get_dir()
         title_worm.move()
         title_worm.draw(screen)
+        
     elif state == 'game':
         screen.blit(font1.render(str(score), True, (0, 0, 0)),
                                  (15, SCREEN_SIZE[1] - 30))
         game_worm.draw(screen)
         food.draw(screen)
+        # Run collision detection
         if game_worm.collide(food) == 'fail':
             state = 'end'
+            
         elif game_worm.collide(food) == 'eat':
             food = worm_obj.Food()
             game_worm.eat()
+            # TODO: Can I do this with a generator thingy?
             for segment in game_worm.worm_list:
                 while segment.outline.colliderect(food.outline):
                     food = worm_obj.Food()
             score += 10
-            delay -= 5
+            delay -= 1
+            pygame.time.set_timer(USEREVENT+1, delay)
             bite.play()
             
             
@@ -90,11 +105,7 @@ while True:
                                  (30, SCREEN_SIZE[1] - 30))
         game_worm.draw(screen)
         food.draw(screen)
-                
-            
-            
-
-        
+                 
         
     pygame.display.update()
 
--- worm_obj.py
+++ worm_obj.py
@@ -1,3 +1,4 @@
+# Objects for Worm game
 import pygame
 import random
 from pygame.locals import *
@@ -5,7 +6,17 @@ from pygame.locals import *
 SCREEN_SIZE = (300, 300)
 
 class Segment:
+    """A 'block' that forms part of the Worm's body."""
+    
     def __init__(self, x, y):
+        """Initialize the Segement.
+
+        Keyword arugments:
+        x -- the left edge of the Segment
+        y -- the top edge of the Segment
+
+        """
+        
         self.x = x
         self.y = y
         self.box = pygame.Rect(x+1, y+1, 8, 8)
@@ -16,6 +27,8 @@ class Segment:
         pygame.draw.rect(surf, (0, 255, 100), self.box)
         
 class Food:
+    """Create a randomly-placed block of food."""
+    
     def __init__(self):
         self.x = random.randint(0, (SCREEN_SIZE[0] - 10)/10) * 10
         self.y = random.randint(0, (SCREEN_SIZE[1] - 10)/10) * 10
@@ -26,6 +39,7 @@ class Food:
 
 class Worm:
     def __init__(self):
+        #Create a worm of length 6
         self.worm_list = []
         x = random.randint(6, SCREEN_SIZE[0]/10) * 10
         y = random.randint(0, SCREEN_SIZE[1]/10) * 10
@@ -41,6 +55,8 @@ class Worm:
     def move(self):
         head = self.worm_list[0]
         self.worm_list.pop()
+        
+        #Append new segment in direction of motion
         if self.direction == 'up':
             self.worm_list.insert(0, Segment(head.x, head.y - 10))
         elif self.direction == 'down':
@@ -52,6 +68,9 @@ class Worm:
 
     def change_dir(self):
         pressed_keys = pygame.key.get_pressed()
+        # != bit is there to prevent Worm from going straight backwards.
+        # Do not replace with, e.g. if self.direction == 'up'.
+        # This will break with rapid inputs.
         if pressed_keys[K_DOWN]:
             if self.worm_list[0].x != self.worm_list[1].x:
                 self.direction = 'down'
@@ -67,18 +86,24 @@ class Worm:
 
     def collide(self, food):
         head = self.worm_list[0]
+        # Return 'fail' if worm goes outside bounds
         if head.x <0 or head.y <0 or head.x + 10 > SCREEN_SIZE[0]:
             return 'fail'
         if head.y + 10 > SCREEN_SIZE[1]:
             return 'fail'
+        # Return 'fail' if worm touches itself.
         for segment in self.worm_list[1:]:
             if head.outline.colliderect(segment.outline):
                 return 'fail'
+        # Note if worm eats food
         if head.outline.colliderect(food.outline):
             return 'eat'
         return 'clear'
 
     def eat(self):
+        # Inserts a head head in front of worm, making it grow.
+        #TODO: append new segment to tail instead of head so
+        #movement not jerky
         head = self.worm_list[0]
         if self.direction == 'up':
             x = head.x
@@ -96,6 +121,8 @@ class Worm:
         self.worm_list.insert(0, new_head)
 
 class TitleWorm(Worm):
+    #This is a subclass of the playable worm. It just rotates around
+    #the main text.
     def __init__(self):
         self.worm_list = []
         self.direction = 'right'
